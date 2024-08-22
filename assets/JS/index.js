aa1d76dd8adef2e239e8f88c8733afbccd9b9909
Juan Barbat <barbat.juan@gmail.com>
2024-08-22 14:15:31 -0300

    tercera entrega

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,106 +1,86 @@
-// let precioTradicional = 1500;
-// let precioSteel = 1300;
-// let precioWood = 1000;
-// function calculaPrecioPorMetro(metros, costoPorMetro) {
-//   return metros * costoPorMetro;
-// } //calculamos metros x costo por metro
+document.addEventListener('DOMContentLoaded', function () {
+  // Configuración inicial
+  let titulo = document.getElementById("titulo");
+  titulo.innerHTML = "Raciones Caninas Uruguay";
+  let footerP = document.getElementById("footer");
+  const actualYear = new Date().getFullYear();
+  footerP.innerHTML = "Todos los derechos reservados ;) " + actualYear;
 
-// let metros = prompt("Metros para el presupuesto "); //pedimos al usuario cantidad de metros
+  // Datos de productos
+  const producto = [
+    { id: 1, nombreProducto: "Equilibrio TR", tipoMascota: "Perro", etapaVida: "Adulto", precio: 3490 },
+    { id: 2, nombreProducto: "Equilibrio CH", tipoMascota: "Perro", etapaVida: "Cachorro", precio: 3790 },
+    { id: 3, nombreProducto: "Natural Choice", tipoMascota: "Gato", etapaVida: "Adulto", precio: 1490 },
+    { id: 4, nombreProducto: "Full Nutrition", tipoMascota: "Perro", etapaVida: "Adulto", precio: 2190 },
+    { id: 5, nombreProducto: "Equilibrio CH", tipoMascota: "Perro", etapaVida: "Cachorro", precio: 3790 },
+    { id: 6, nombreProducto: "Equilibrio Raza Grande", tipoMascota: "Perro", etapaVida: "Adulto", precio: 3590 },
+    { id: 7, nombreProducto: "Equilibrio CH Raza Grande", tipoMascota: "Perro", etapaVida: "Cachorro", precio: 3890 },
+    { id: 8, nombreProducto: "Natural Choice", tipoMascota: "Perro", etapaVida: "Adulto", precio: 2490 },
+    { id: 9, nombreProducto: "Full Nutrition", tipoMascota: "Perro", etapaVida: "Cachorro", precio: 2090 },
+    { id: 10, nombreProducto: "Frost", tipoMascota: "Gato", etapaVida: "Cachorro", precio: 1890 },
+    { id: 11, nombreProducto: "Frost", tipoMascota: "Gato", etapaVida: "Adulto", precio: 1790 },
+    { id: 12, nombreProducto: "Frost", tipoMascota: "Perro", etapaVida: "Adulto", precio: 2790 }
+  ];
 
-// metros = parseFloat(metros);
+  // buscar productos
+  const buscadorInput = document.createElement('input');
+  buscadorInput.type = 'text';
+  buscadorInput.id = 'buscador-input';
+  buscadorInput.placeholder = 'Busca tu producto...';
 
-// let costoPorMetro; //Definimos de manera global la variable, la había definido dentro del if ;(
-// if (metros > 0) {
-//   let tipoDeConstruccion = prompt(
-//     "Ingrese el tipo de construcción (tradicional, steel o wood)"
-//   );
-//   switch (tipoDeConstruccion) {
-//     case "tradicional":
-//       costoPorMetro = precioTradicional;
-//       break;
-//     case "steel":
-//       costoPorMetro = precioSteel;
-//       break;
-//     case "wood":
-//       costoPorMetro = precioWood;
-//       break;
-//     default:
-//       costoPorMetro = undefined || null;
-//       console.error("El tipo de construcción no es válido.");
-//   }
-// }
-// const formaDePago = prompt(
-//   "Ingrese forma de pago, contado o 12 cuotas sin recargo"
-// );
+  const mainElement = document.querySelector('main');
+  const productosContainer = document.getElementById('productContainer');
+  mainElement.insertBefore(buscadorInput, productosContainer);
 
-// let costoTotal;
-// if (metros > 0) {
-//   costoTotal = calculaPrecioPorMetro(metros, costoPorMetro);
-//   console.log("Este es el precio total estimado en USD " + costoTotal);
-// } else {
-//   if (metros <= 0) {
-//     console.error("No se puede calcular");
-//   } else {
-//     console.error("Error");
-//   }
-// }
-
-// if (formaDePago === "contado") {
-//   costoTotal = costoTotal * 0.9;
-//   console.log("El precio final pagando contado es de USD" + costoTotal);
-// } else if (formaDePago === "12 cuotas") {
-//   costoTotal = costoTotal / 12;
-//   console.log("El valor de cada cuota es de es de USD" + costoTotal);
-// } else {
-//   console.error("Opción inválida");
-// }
-
-let titulo = document.getElementById("titulo");
-titulo.innerHTML = "Raciones Caninas Uruguay";
-let footerP = document.getElementById("footer");
-let footer = document.createElement("p")
-footerP.innerHTML = "Todos los derechos reservados ;)";
-
-
-console.log(footerP);
-///////////////////////////////
-const producto = [];
-
-producto.push(
-  { nombreProducto: "Equilibrio TR", tipoMascota: "Perro", etapaVida: "Adulto", precio: 3490 },
-  { nombreProducto: "Equilibrio CH", tipoMascota: "Perro", etapaVida: "Cachorro", precio: 3790 },
-  { nombreProducto: "Natural Choice", tipoMascota: "Gato", etapaVida: "Adulto", precio: 1490 },
-  { nombreProducto: "Full Nutrition", tipoMascota: "Perro", etapaVida: "Adulto", precio: 2190 },
-  { nombreProducto: "Equilibrio CH", tipoMascota: "Perro", etapaVida: "Cachorro", precio: 3790 },
-  { nombreProducto: "Equilibrio Raza Grande", tipoMascota: "Perro", etapaVida: "Adulto", precio: 3590 },
-  { nombreProducto: "Equilibrio CH Raza Grande", tipoMascota: "Perro", etapaVida: "Cachorro", precio: 3890 },
-  { nombreProducto: "Natural Choice", tipoMascota: "Perro", etapaVida: "Adulto", precio: 2490 },
-  { nombreProducto: "Full Nutrition", tipoMascota: "Perro", etapaVida: "Cachorro", precio: 2090 },
-  { nombreProducto: "Frost", tipoMascota: "Gato", etapaVida: "Cachorro", precio: 1890 },
-  { nombreProducto: "Frost", tipoMascota: "Gato", etapaVida: "Adulto", precio: 1790 },
-  { nombreProducto: "Frost ", tipoMascota: "Perro", etapaVida: "Adulto", precio: 2790 }
-);
-
-function buscarProducto() {
-  const productoBuscado = prompt("Ingrese producto a buscar");
-  const productoEncontrado = producto.filter(producto =>
-    producto.nombreProducto.toLowerCase().includes(productoBuscado.toLowerCase())
-  );
-  if (productoEncontrado) {
-    console.log(productoEncontrado);
-  } else {
-    console.error("Producto no encontrado");
+  function renderizarProductos(productosFiltrados = producto) {
+    productosContainer.innerHTML = '';
+    productosFiltrados.forEach(prod => {
+      let productoDiv = document.createElement("div");
+      productoDiv.className = "productoItem";
+      productoDiv.innerHTML = `
+        <h3>${prod.nombreProducto}</h3>
+        <p>Tipo de Mascota: ${prod.tipoMascota}</p>
+        <p>Etapa de Vida: ${prod.etapaVida}</p>
+        <p>Precio: $${prod.precio}</p>
+        <button class="agregar-btn" data-id="${prod.id}">Agregar</button>
+      `;
+      productosContainer.appendChild(productoDiv);
+    });
   }
-}
-buscarProducto();
 
-const ahora = new Date();
-const hora = ahora.getHours();
+  renderizarProductos();
 
-if (hora < 12) {
-  console.log("Hola Sofia! espero que tengas un lindo día");
-} else if (hora < 18) {
-  console.log("Hola Sofia! espero que tu día esté siendo lindo");
-} else {
-  console.log("Hola Sofia! espero que hayas tenido un lindo día");
-}
+  // evento busqueda
+  buscadorInput.addEventListener('input', function () {
+    const textoBusqueda = buscadorInput.value.toLowerCase();
+    const productosFiltrados = producto.filter(prod =>
+      prod.nombreProducto.toLowerCase().includes(textoBusqueda)
+    );
+    renderizarProductos(productosFiltrados);
+  });
+
+  // gregar productos al carrito
+  productosContainer.addEventListener('click', (event) => {
+    if (event.target && event.target.classList.contains('agregar-btn')) {
+      let productoId = parseInt(event.target.getAttribute('data-id'), 10);
+      let productoItem = producto.find(p => p.id === productoId);
+
+      if (productoItem) {
+        let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+        carrito.push(productoItem);
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+      }
+    }
+  });
+
+  const ahora = new Date();
+  const hora = ahora.getHours();
+
+  if (hora < 12) {
+    console.log("Hola! espero que tengas un buen día");
+  } else if (hora < 18) {
+    console.log("Hola! espero que tu día esté siendo genial");
+  } else {
+    console.log("Hola! espero que hayas tenido un buen día");
+  }
+});
---
+++ assets/JS/storage.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const productosContainer = document.getElementById("productContainer");
+
+    productosContainer.addEventListener('click', (event) => {
+        if (event.target && event.target.classList.contains('agregar-btn')) {
+            let productoId = parseInt(event.target.getAttribute('data-id'), 10);
+            let productoItem = producto.find(p => p.id === productoId); // Usa el mismo nombre para la variable
+
+            if (productoItem) {
+                let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+                carrito.push(productoItem);
+                localStorage.setItem('carrito', JSON.stringify(carrito));
+            }
+        }
+    });
+});
